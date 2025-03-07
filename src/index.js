d6ca1ad927abec1d629d9072292fddc785bfda3a
jonathan <jaadams12@outlook.com>
2025-03-07 16:28:44 +0000

    package.json

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const container = document.getElementById('root');
 if (container) {
   const root = ReactDOM.createRoot(container);
